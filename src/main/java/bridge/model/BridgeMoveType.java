39484470a9e148406ec32063b6258fba5fd676cd
ttaehee <ttaehee1105@gmail.com>
2023-03-24 22:25:10 +0900

    Feat : 다리 이동 타입 및 예외 메시지 구현

---
+++ src/main/java/bridge/model/BridgeMoveType.java
@@ -0,0 +1,40 @@
+package bridge.model;
+
+import static bridge.exception.ExceptionMessage.WRONG_BRIDGE_TYPE_EXCEPTION;
+
+import java.util.Arrays;
+
+public enum BridgeMoveType {
+	UP(0, "U"),
+	DOWN(1, "D");
+
+	private final int code;
+	private final String direction;
+
+	BridgeMoveType(int code, String direction) {
+		this.code = code;
+		this.direction = direction;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public static BridgeMoveType of(int bridgeCode) {
+		return Arrays.stream(BridgeMoveType.values())
+				.filter(bridge -> bridge.code == bridgeCode)
+				.findFirst()
+				.orElseThrow(()-> new IllegalArgumentException(WRONG_BRIDGE_TYPE_EXCEPTION.getMessage()));
+	}
+
+	public static BridgeMoveType of(String bridgeDirection) {
+		return Arrays.stream(BridgeMoveType.values())
+				.filter(bridge -> bridge.direction.equals(bridgeDirection))
+				.findFirst()
+				.orElseThrow(()-> new IllegalArgumentException(WRONG_BRIDGE_TYPE_EXCEPTION.getMessage()));
+	}
+}
