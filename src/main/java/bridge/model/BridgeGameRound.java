2535b9f77533ac66df60e8e587ba639b2878454c
ttaehee <ttaehee1105@gmail.com>
2023-03-25 00:39:59 +0900

    Feat : 게임 라운드 저장 기능 구현

---
+++ src/main/java/bridge/model/BridgeGameRound.java
@@ -0,0 +1,14 @@
+package bridge.model;
+
+public class BridgeGameRound {
+
+	private int round = 1;
+
+	public int getRound() {
+		return this.round;
+	}
+
+	public void plusRound() {
+		round++;
+	}
+}
